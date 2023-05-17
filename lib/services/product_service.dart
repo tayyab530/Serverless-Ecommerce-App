fc118fa2b5b03596aa50b32aff29dfcfad3de6eb
Tayyab Ahmed <mmtayyab530@gmail.com>
2023-05-18 00:20:37 +0500

    Change Compile SDK to 33

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<List<dynamic>> fetchProducts() async {
-  var apiUrl = "YOUR_API_HERE";
+  var apiUrl = "https://en3hu5n1lk.execute-api.eu-north-1.amazonaws.com/items";
   try {
     final response = await http.get(Uri.parse(apiUrl));
     if(response.statusCode == 200){
@@ -14,6 +14,6 @@ Future<List<dynamic>> fetchProducts() async {
       return [];
   } catch (error) {
     print('Error fetching products: $error');
-    rethrow;
+    return [];
   }
 }
